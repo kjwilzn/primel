c4f64e31ceb303ff9e18faecf04c31147e4bc15b
David Lawrence Miller <github@ninepointeightone.net>
2022-01-20 21:32:16 +0000

    minimal working nonsense

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -45,39 +45,21 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
   return (
     <div>
       <div className="flex justify-center mb-1">
-        <Key value="Q" onClick={onClick} status={charStatuses['Q']} />
-        <Key value="W" onClick={onClick} status={charStatuses['W']} />
-        <Key value="E" onClick={onClick} status={charStatuses['E']} />
-        <Key value="R" onClick={onClick} status={charStatuses['R']} />
-        <Key value="T" onClick={onClick} status={charStatuses['T']} />
-        <Key value="Y" onClick={onClick} status={charStatuses['Y']} />
-        <Key value="U" onClick={onClick} status={charStatuses['U']} />
-        <Key value="I" onClick={onClick} status={charStatuses['I']} />
-        <Key value="O" onClick={onClick} status={charStatuses['O']} />
-        <Key value="P" onClick={onClick} status={charStatuses['P']} />
-      </div>
-      <div className="flex justify-center mb-1">
-        <Key value="A" onClick={onClick} status={charStatuses['A']} />
-        <Key value="S" onClick={onClick} status={charStatuses['S']} />
-        <Key value="D" onClick={onClick} status={charStatuses['D']} />
-        <Key value="F" onClick={onClick} status={charStatuses['F']} />
-        <Key value="G" onClick={onClick} status={charStatuses['G']} />
-        <Key value="H" onClick={onClick} status={charStatuses['H']} />
-        <Key value="J" onClick={onClick} status={charStatuses['J']} />
-        <Key value="K" onClick={onClick} status={charStatuses['K']} />
-        <Key value="L" onClick={onClick} status={charStatuses['L']} />
+        <Key value="1" onClick={onClick} status={charStatuses['1']} />
+        <Key value="2" onClick={onClick} status={charStatuses['2']} />
+        <Key value="3" onClick={onClick} status={charStatuses['3']} />
+        <Key value="4" onClick={onClick} status={charStatuses['4']} />
+        <Key value="5" onClick={onClick} status={charStatuses['5']} />
+        <Key value="6" onClick={onClick} status={charStatuses['6']} />
+        <Key value="7" onClick={onClick} status={charStatuses['7']} />
+        <Key value="8" onClick={onClick} status={charStatuses['8']} />
+        <Key value="9" onClick={onClick} status={charStatuses['9']} />
+        <Key value="0" onClick={onClick} status={charStatuses['0']} />
       </div>
       <div className="flex justify-center">
         <Key width={65.4} value="ENTER" onClick={onClick}>
           Enter
         </Key>
-        <Key value="Z" onClick={onClick} status={charStatuses['Z']} />
-        <Key value="X" onClick={onClick} status={charStatuses['X']} />
-        <Key value="C" onClick={onClick} status={charStatuses['C']} />
-        <Key value="V" onClick={onClick} status={charStatuses['V']} />
-        <Key value="B" onClick={onClick} status={charStatuses['B']} />
-        <Key value="N" onClick={onClick} status={charStatuses['N']} />
-        <Key value="M" onClick={onClick} status={charStatuses['M']} />
         <Key width={65.4} value="DELETE" onClick={onClick}>
           Delete
         </Key>
--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -62,42 +62,42 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Guess the WORDLE in 6 tries. After each guess, the color
+                      Guess the prime in 6 tries. After each guess, the color
                       of the tiles will change to show how close your guess was
-                      to the word.
+                      to the prime.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="W" status="correct" />
-                      <Cell value="E" />
-                      <Cell value="A" />
-                      <Cell value="R" />
-                      <Cell value="Y" />
+                      <Cell value="7" status="correct" />
+                      <Cell value="1" />
+                      <Cell value="4" />
+                      <Cell value="2" />
+                      <Cell value="9" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter W is in the word and in the correct spot.
+                      The 7 is in the prime and in the correct spot.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="P" />
-                      <Cell value="I" />
-                      <Cell value="L" status="present" />
-                      <Cell value="O" />
-                      <Cell value="T" />
+                      <Cell value="7" />
+                      <Cell value="3" />
+                      <Cell value="0" status="present" />
+                      <Cell value="6" />
+                      <Cell value="1" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter L is in the word but in the wrong spot.
+                      The 0 is in the prime but in the wrong spot.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
-                      <Cell value="V" />
-                      <Cell value="A" />
-                      <Cell value="G" />
-                      <Cell value="U" status="absent" />
-                      <Cell value="E" />
+                      <Cell value="2" />
+                      <Cell value="0" />
+                      <Cell value="6" />
+                      <Cell value="3" status="absent" />
+                      <Cell value="9" />
                     </div>
                     <p className="text-sm text-gray-500">
-                      The letter U is not in the word in any spot.
+                      The 3 is not in the prime in any spot.
                     </p>
                   </div>
                 </div>
