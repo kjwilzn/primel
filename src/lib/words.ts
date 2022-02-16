9fedc6ca19a6e70f4bc5f2d27d52bc799c7976f8
kjwilzn <89235605+kjwilzn@users.noreply.github.com>
2022-02-15 22:14:00 -0800

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -3,8 +3,7 @@ import { VALIDGUESSES } from '../constants/validGuesses'
 
 export const isWordInWordList = (word: string) => {
   return (
-    WORDS.includes(word.toLowerCase()) ||
-    VALIDGUESSES.includes(word.toLowerCase())
+    true
   )
 }
 
